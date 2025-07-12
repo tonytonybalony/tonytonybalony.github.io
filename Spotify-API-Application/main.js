dcb6f83fcbab4d01a27a26552851b41b31751ad5
Tony <toto515891@gmail.com>
2025-07-13 00:21:22 +0800

    Site updated: 2025-07-13 00:21:21

---
+++ Spotify-API-Application/main.js
@@ -0,0 +1,39 @@
+// Get top 5 tracks from Spotify using the Web API
+
+// Authorization token that must have been created previously. See : https://developer.spotify.com/documentation/web-api/concepts/authorization
+const token = 'BQDtkn2A0-K0VPAWQPtgKJXWKmSaGbQ1GwGCaJnYbvVxxsyZTgxuh1pL-r2iK2cJpCPcYqhuxPXki-Linh5yKbFFckpBQOlYc5p0WRGfCboz-zC1aNPY_VrWCJpYiwrh6_DggJWC40D4VIS-YJAumsszD7BCMwl9I3LejSV7kuKdgxO8QnhRL0QN4iUm6wkCj14r06eZArHcrKbHBrCeT-c09T-qzClY8-8SEsgYV5KHSuRyFyXa8GM-R4GqTYjEiw2X2VwC8QRJSfIP32mNxXkXrOxyq2E5CiTz2grdbrLopoDMl8lvCElbBG2V6COVgOUP';
+async function fetchWebApi(endpoint, method, body) {
+  const res = await fetch(`https://api.spotify.com/${endpoint}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    method,
+    body:JSON.stringify(body)
+  });
+  return await res.json();
+}
+
+const tracksUri = [
+  'spotify:track:1Qrg8KqiBpW07V7PNxwwwL','spotify:track:6AI3ezQ4o3HUoP6Dhudph3','spotify:track:3oDkdAySo1VQQG0ptV7uwa','spotify:track:3LtpKP5abr2qqjunvjlX5i','spotify:track:3OxL6MuctgZp1e0zxoAZhH'
+];
+
+async function createPlaylist(tracksUri){
+  const { id: user_id } = await fetchWebApi('v1/me', 'GET')
+
+  const playlist = await fetchWebApi(
+    `v1/users/${user_id}/playlists`, 'POST', {
+      "name": "My top tracks playlist",
+      "description": "Playlist created by the tutorial on developer.spotify.com",
+      "public": false
+  })
+
+  await fetchWebApi(
+    `v1/playlists/${playlist.id}/tracks?uris=${tracksUri.join(',')}`,
+    'POST'
+  );
+
+  return playlist;
+}
+
+const createdPlaylist = await createPlaylist(tracksUri);
+console.log(createdPlaylist.name, createdPlaylist.id);
